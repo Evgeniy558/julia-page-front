6dfd1f052c3692a80fbdfe52fb388816ac5d8aed
Evgenij Prishchepov <prishchepovee@gmail.com>
2024-01-18 23:45:57 +0300

    add slide effect

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-
 import "./css/normalize.css";
 import "./css/master.css";
 import "./css/fonts.css";
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,13 +1,18 @@
 import LeftSlide from "../../components/Slides/LeftSlide/LeftSlide";
 import RightSlide from "../../components/Slides/RightSlide/RightSlide";
+import css from "./Home.module.css";
 // import css from "./Home.module.css";
 const Home = () => {
   return (
-    <secrion>
-      <h1>Home page</h1>
-      <LeftSlide />
-      <RightSlide />
-    </secrion>
+    <>
+      <section className={css.section}>
+        <h1>Home page</h1>
+        <LeftSlide />
+      </section>
+      <section>
+        Test section asdaas asdaasd asdasdasd asdaasd <p>test</p>
+      </section>
+    </>
   );
 };
 export default Home;
