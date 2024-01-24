0e3af564973d10cda358136bf7f991be298dbcd7
Evgenij Prishchepov <prishchepovee@gmail.com>
2024-01-24 10:58:01 +0300

    add styles in Slide component

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,6 +2,7 @@ import Footer from "../../components/Footer/Footer";
 import Slide from "../../components/Slide/Slide";
 import imageTest1 from "../../images/test1.jpg";
 import imageTest2 from "../../images/test2.jpg";
+import imageTest3 from "../../images/test3.jpg";
 import css from "./Home.module.css";
 
 const Home = () => {
@@ -12,13 +13,24 @@ const Home = () => {
         <Slide
           urlImg={imageTest1}
           slideTitle={"Test Title slide_1"}
-          slideText={"Titile slide_1 test"}
+          slideText={
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis sit accusantium cumque aliquid assumenda. Reiciendis numquam accusantium sit, minima fugit tenetur ad doloremque aliquam, voluptatum totam, nemo aliquid mollitia atque!"
+          }
         />
         <Slide
           urlImg={imageTest2}
           slideType="right"
           slideTitle={"Test Title slide_2"}
-          slideText={"Titile slide_2 test"}
+          slideText={
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis sit accusantium cumque aliquid assumenda. Reiciendis numquam accusantium sit"
+          }
+        />
+        <Slide
+          urlImg={imageTest3}
+          slideTitle={"Test Title slide_3"}
+          slideText={
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis sit accusantium cumque aliquid assumenda. Reiciendis numquam accusantium sit, minima fugit tenetur ad doloremque aliquam, voluptatum totam, nemo aliquid mollitia atque!"
+          }
         />
       </section>
       <Footer />
